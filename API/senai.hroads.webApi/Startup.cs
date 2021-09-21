03e6e43bd396b9f2b0c959bc8fa72f94479506e3
ViniciusVianazin <viniciusniviana@gmail.com>
2021-09-21 19:56:20 -0300

    finalizando

--- API/senai.hroads.webApi/Startup.cs
+++ API/senai.hroads.webApi/Startup.cs
@@ -16,6 +16,7 @@ namespace senai.hroads.webApi_
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,9 +31,16 @@ namespace senai.hroads.webApi_
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
+                if (env.IsDevelopment())
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    app.UseDeveloperExceptionPage();
+                }
+
+                app.UseRouting();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
                 });
             });
         }
